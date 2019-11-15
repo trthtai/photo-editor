7be977e1086f88510df1ca97f7aec7ffd0184459
Trương Thuận Tài <trthtai92@gmail.com>
2019-11-15 17:52:47 +0700

    - Add Carthage support.

---
+++ Carthage/Build/iOS/Photo_Editor_framework.framework/Headers/Photo_Editor_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Photo_Editor_Framework.h
+//  Photo Editor Framework
+//
+//  Created by Vladimir Tyrin on 04/12/2018.
+//  Copyright © 2018 Mohamed Hamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Photo_Editor_Framework.
+FOUNDATION_EXPORT double Photo_Editor_FrameworkVersionNumber;
+
+//! Project version string for Photo_Editor_Framework.
+FOUNDATION_EXPORT const unsigned char Photo_Editor_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Photo_Editor_Framework/PublicHeader.h>
+
+
